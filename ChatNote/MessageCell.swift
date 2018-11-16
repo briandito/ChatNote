2f5632d7477417d61d752ec8ce391c425480dffa
Briandito Priambodo <briandito.p@gmail.com>
2018-11-16 18:37:15 +0800

    add ui

---
+++ ChatNote/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  ChatNote
+//
+//  Created by Briandito Priambodo on 16/11/18.
+//  Copyright © 2018 Briandito Priambodo. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
