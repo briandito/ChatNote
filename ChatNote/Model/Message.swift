9a7296b92c67353d0a8a6e0c71bcc0e34ec5df8a
Briandito <briandito.p@gmail.com>
2018-11-17 16:02:57 +0800

    added iboutlet references for the custom message cell

---
+++ ChatNote/Model/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  ChatNote
+//
+//  Created by Briandito Priambodo on 17/11/18.
+//  Copyright © 2018 Briandito Priambodo. All rights reserved.
+//
+
+class Message {
+    
+    //MARK: Message date and message body variable
+    
+    var date: String = ""
+    var messageBody: String = ""
+    
+}
